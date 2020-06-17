9bf9d905ba0c1c18a511d056ed9079c05001d046
yangkui <752544765@qq.com>
2020-06-17 17:59:08 +0800

    Fix the lack of JavaFX components in direct click to run

---
+++ app/src/main/java/com/openjfx/database/app/Launcher.java
@@ -0,0 +1,13 @@
+package com.openjfx.database.app;
+
+/**
+ * Launcher DatabaseFx application
+ *
+ * @author yangkui
+ * @since 1.0
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        DatabaseFX.main(args);
+    }
+}
