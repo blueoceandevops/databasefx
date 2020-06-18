78cd5f7bb5d8eea34ceb32c174bd1344bde87dec
yangkui <752544765@qq.com>
2020-06-18 22:12:34 +0800

    Write application related information to MANIFEST.MF in

--- app/src/main/java/com/openjfx/database/app/controller/AboutController.java
+++ app/src/main/java/com/openjfx/database/app/controller/AboutController.java
@@ -1,6 +1,15 @@
 package com.openjfx.database.app.controller;
 
 import com.openjfx.database.app.BaseController;
+import com.openjfx.database.app.utils.AssetUtils;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * about application stage
@@ -10,4 +19,41 @@ import com.openjfx.database.app.BaseController;
  */
 public class AboutController extends BaseController<Void> {
 
+    @FXML
+    private Label name;
+
+    @FXML
+    private Label author;
+
+    @FXML
+    private Label version;
+
+    @FXML
+    private Label website;
+
+
+    @Override
+    public void init() {
+        var future = CompletableFuture.supplyAsync(() -> {
+            Map<String, String> map;
+            try {
+                map = AssetUtils.loadManifest();
+            } catch (IOException e) {
+                logger.error("load manifest attr failed", e);
+                throw new RuntimeException(e);
+            }
+            return map;
+        });
+        future.whenComplete((attr, t) -> {
+            if (t != null) {
+                return;
+            }
+            Platform.runLater(() -> {
+                name.setText(attr.get("App-Name"));
+                author.setText(attr.get("App-Author"));
+                version.setText(attr.get("Manifest-Version"));
+                website.setText(attr.get("App-Website"));
+            });
+        });
+    }
 }
